2509ab76e4c30e42ffab7cb02c4eff07d59eb750
radearsr <radea.surya4@gmail.com>
2025-10-21 16:35:54 +0700

    fix: build gradle for compatible with env android_sdk_root

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,27 @@
+import java.util.Properties
+
+val localPropertiesFile = rootProject.file("local.properties")
+var sdkDir: String? = null
+
+if (localPropertiesFile.exists()) {
+    val properties = Properties()
+    localPropertiesFile.inputStream().use { properties.load(it) }
+    sdkDir = properties.getProperty("sdk.dir")
+}
+
+if (sdkDir.isNullOrEmpty()) {
+    sdkDir = System.getenv("ANDROID_SDK_ROOT")
+}
+
+if (sdkDir.isNullOrEmpty()) {
+    throw GradleException(
+        "SDK location not found. Please set ANDROID_SDK_ROOT or create local.properties with sdk.dir."
+    )
+}
+
+// Set ke gradle agar dikenali Android plugin
+System.setProperty("android.home", sdkDir)
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
